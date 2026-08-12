4a8d52cc01062730a754573c838592279650cd90
Daniela Cardozo <mdanielacardozo@gmail.com>
2026-08-11 21:03:24 -0300

    Agregamos conceptos iniciales de variables

---
+++ js/01-variables.js
@@ -0,0 +1,19 @@
+//esto es un comentario de una linea
+
+/* comentario multilinea */
+
+let precioCompra;
+
+let descuento;
+
+let nombre;
+
+nombre = "Alejandro";
+
+console.log("Hola mundo");
+
+console.log(nombre);
+
+
+
+
