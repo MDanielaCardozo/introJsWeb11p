8fc7e6d897553242c9c26df20ceba8a2000b750e
Daniela Cardozo <mdanielacardozo@gmail.com>
2026-08-18 20:15:15 -0300

    agregue ejercicio de switch

---
+++ js/03-switch.js
@@ -0,0 +1,52 @@
+// Cree un programa al estilo de un cajero automatico con las siguientes opciones:
+// 1- consultar el saldo
+// 2- ingresar dinero
+// 3- extraer dinero
+
+let saldo = 100000;
+
+const opcion = prompt(
+  "Ingresa una opcion: 1-consultar el sado - 2-Ingresar dinero - 3-extraer dinero",
+);
+
+switch (opcion) {
+  case "saldo":
+  case "1":
+  case 1:
+    //aqui agrego todas las lineas de codigo que necesito para la opcion 1 
+    alert(`Tu saldo es $ ${saldo}`)
+    break;
+  case "deposito":
+  case "2":
+  case 2:
+    const deposito = parseFloat(prompt("Ingresa el monto que deseas depositar"));
+    saldo = saldo + deposito;
+    alert(`Ingresaste $ ${deposito}, tu saldo actual es $ ${saldo}`);
+    break
+  case "extraer":
+  case "3":
+  case 3:
+         const extraccion = parseFloat(prompt("Ingresa el monto a extraer"));
+         //verificar si el usuario puede extraer el monto solicitado
+         if (extraccion <= saldo) {
+            saldo = saldo - extraccion;
+            alert(`Retiraste $${extraccion}, tu saldo actual es $${saldo}`)
+         } else {
+            alert("Fondos insuficientes")
+         }   
+         break
+  default:
+    alert("Ingresaste una opcion erronea")
+    break;
+}
+
+//Ejemplo de lo que debemos evitar
+/* if (opcion === 1) {
+  alert("Consultar saldo");
+} else if (opcion === 2) {
+  alert("Aqui tenemos que ingresar dinero");
+} else if (opcion === 3) {
+  alert("Aqui tenemos que extraer dinero");
+} else {
+  alert("Ingresar una opcion valida");
+} */
